425a553b085ffbb40f093774d32ed53330e9d562
Chris Angelico <rosuav@gmail.com>
2012-10-18 17:07:19 +1100

    Add a README and two example config files

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration. Change these variables to suit your system.
+usevnc=False  # Set to True to use a VNC connection to send keys. Requires a VNC server on the same computer, running with authentication disabled.
+invokecmd=None # By default, attempt to detect an invocation method.
+# Or set it to a full command such as this. Use %s for the file name.
+# invokecmd='start "Movie Player" "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" -f "%s"'
+dvdcmd=None # By default, DVD folders will use the same invocation method as ordinary files (whether detected or explicit command).
+# dvdcmd='start "Movie Player" "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" -f "%s"'
+dvdcmd='vlc -f dvdsimple://"%s" >/dev/null 2>/dev/null &'
+abortcmd='killall totem; killall vlc' # Execute this to kill all video players. Hmm. There's a pun in there, or I'm much mistaken.
+basepath=r"/video"  # Base path to all videos. Note that this does not chroot or anything, and is not guaranteed to prevent all possible ways "out" of the "jail".
+# End of config options. Below here shouldn't need to be changed.
